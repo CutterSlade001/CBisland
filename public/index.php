e5ef3b943f7761a4147ea2acc74735423649f14d
hellrence <hi@hellrence.me>
2024-12-30 21:02:27 +0100

    Add helper service inclusion in index.php
    Included `helper.php` to load additional functionalities used in the application. This ensures necessary helpers are available for rendering and processing.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 require dirname(__DIR__) . '/config/config.php';
-
+require APP_ROOT         . '/services/helper.php';
 
 render('index.php', [
     'msg' => 'Works !',
