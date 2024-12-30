e8f123526f25507f3d7e403a24ff98aa9c7a3b96
hellrence <hi@hellrence.me>
2024-12-30 17:28:34 +0100

    Minor Updates to render function.

--- services/helper.php
+++ services/helper.php
@@ -3,14 +3,20 @@
 /**
  * Renders the specified template with the provided data.
  *
- * @param string $template The name of the template file to render.
- * @param array  $data     An associative array of data to be used within the template. Optional.
+ * @param string     $template The name of the template file to render.
+ * @param array|null $data     An associative array of data to be used within the template. Optional.
  *
  * @return void
+ * @throws Exception
  */
-function render(string $template, array $data = []): void
+function render(string $template, ?array $data = []): void
 {
     extract($data);
 
-    require APP_TEMPLATES . '/' . $template;
+    $_tmpl = APP_TEMPLATES . DIRECTORY_SEPARATOR . $template;
+
+    if (! file_exists($_tmpl))
+        throw new Exception('Template not found: ' . $_tmpl);
+
+    require $_tmpl;
 }
